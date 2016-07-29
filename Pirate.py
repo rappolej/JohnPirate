619803d4f5f55ab7d263ac57765c100a6e3bc980
rappolej <rappolej@rappolej-lm3.local>
2016-07-29 13:34:35 -0400

    new change

--- Pirate.py
+++ Pirate.py
@@ -8,3 +8,7 @@ for i in range(100):
   with open('treasure.txt') as fh:
   for line in fh:
     print line
+    
+    with open('map.txt', 'w') as fh:
+  for i in range(10):
+    fh.write("Walk " + str(i) + " paces left.")
