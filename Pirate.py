c3e67c8f3d03ef764ed8c6dfca3f91de520bf3d9
rappolej <rappolej@rappolej-lm3.local>
2016-07-29 13:43:27 -0400

    indent fix

--- Pirate.py
+++ Pirate.py
@@ -1,14 +1,14 @@
 
-print "Arr, matey"
+    print "Arr, matey"
 
 
-for i in range(100):
-  print str(i) + " bottles of rum"
+    for i in range(100):
+    print str(i) + " bottles of rum"
   
-  with open('treasure.txt') as fh:
-  for line in fh:
+    with open('treasure.txt') as fh:
+    for line in fh:
     print line
     
     with open('map.txt', 'w') as fh:
-  for i in range(10):
+    for i in range(10):
     fh.write("Walk " + str(i) + " paces left.")
